d868097bf5c258fdaa4feda82f808db74276914b
Rami Nouaili <44841946+raminouaili@users.noreply.github.com>
2025-08-02 14:06:33 -0400

    feat: add signup flow

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { OnboardingComponent } from './auth/onboarding/OnboardingComponent';
 import { LoginComponent } from './auth/login/LoginComponent';
+import { SignupComponent } from './auth/signup/SignupComponent';
 
 export const routes: Routes = [
   { path: 'auth', component: OnboardingComponent },
+  { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/auth', pathMatch: 'full' }
 ];
