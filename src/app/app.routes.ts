1ee8c03c99c9476786f4f8505ef6fe8f54780454
raminouaili <raminouaili@gmail.com>
2025-08-02 13:10:49 -0400

    Interactive UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { OnboardingComponent } from './auth/onboarding/OnboardingComponent';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'auth', component: OnboardingComponent },
+  { path: '', redirectTo: '/auth', pathMatch: 'full' }
+];
